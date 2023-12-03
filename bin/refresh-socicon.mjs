0ead88572a1257dd736679b7817daae8de5e6839
Brian Takita <brian.takita@gmail.com>
2023-12-03 01:09:46 -0500

    format

--- bin/refresh-socicon.mjs
+++ bin/refresh-socicon.mjs
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import { Parser } from 'htmlparser2'
-import { readFile, writeFile } from 'fs/promises'
-import { dirname, join } from 'path'
 import { map, sort } from '@ctx-core/array'
 import { param_r_ } from '@ctx-core/cli-args'
 import { keys } from '@ctx-core/object'
+import { readFile, writeFile } from 'fs/promises'
+import { Parser } from 'htmlparser2'
+import { dirname, join } from 'path'
 await main()
 async function main() {
 	const { dir } = opts_()
